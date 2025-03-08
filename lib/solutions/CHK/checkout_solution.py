c5bc18de5463c67d18de5538f74da71dde9f32b6
sbx_user1051 <sbx_user1051@169.254.10.73>
2025-03-08 00:33:13 +0000

    Sat Mar 08 00:33:13 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,13 +1,34 @@
+import math
+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     prices = {'A': 50, 'B': 30, 'C': 20, 'D': 15}
     offers = {'A': (3, 130), 'B': (2, 45)}
     valid_inputs = ('A', 'B', 'C', 'D')
+    frequency_map = {}
     for s in skus:
         if s not in valid_inputs:
             return -1
+        if s in frequency_map:
+            frequency_map[s] += 1
+        else:
+            frequency_map[s] = 1
 
+    sum = 0
+
+    for s,count in frequency_map.items():
+        if s not in offers:
+            sum += count * prices[s]
+        else:
+            sum += math.floor(count/offers[s][0])*offers[s][1]
+            sum += (count % offers[s][1]) * prices[s]
+    return sum
 
 print(checkout("AAc"))
+print(checkout("AAAB"))
+print(checkout("ABBBBBBAB"))
+print(checkout("AABCD"))
+print(checkout("AAAA"))
+
 
